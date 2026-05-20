d8e73a7e901f22ac773a349dac939fbdb0466efb
Deepu-Builds <dkeer28@gmail.com>
2026-05-20 13:41:35 +0530

    Ensure backend builds before production start

--- backend/dist/scheduler/due-date.scheduler.d.ts
+++
@@ -1,9 +0,0 @@
-import { TasksService } from '../tasks/tasks.service';
-import { NotificationsService } from '../notifications/notifications.service';
-export declare class DueDateScheduler {
-    private tasksService;
-    private notificationsService;
-    private logger;
-    constructor(tasksService: TasksService, notificationsService: NotificationsService);
-    checkDueDates(): Promise<void>;
-}
